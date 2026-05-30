c2edf44049f6ed12be258a8df6a3dfb34c4f3b8f
MooProg <moosaeed.de@gmail.com>
2026-05-30 13:32:43 +0200

    Add shapes program with polymorphism

---
+++ week-03-oop/src/ShapesApp/Main.java
@@ -0,0 +1,15 @@
+package ShapesApp;
+
+public class Main {
+    public static void main (String[] args){
+        Shape[] shapes={
+                new Circle(5),
+                new Rectangle(4, 6),
+                new Triangle(3, 8)
+        };
+        for (Shape s : shapes){
+            s.area();
+            s.describe();
+        }
+    }
+}

fec3947cd0e8a602a1bd9f720858b2b3dd8f2b1d
MooProg <moosaeed.de@gmail.com>
2026-06-01 12:49:15 +0200

    Refactor Shape into abstract class

--- week-03-oop/src/ShapesApp/Main.java
+++ week-03-oop/src/ShapesApp/Main.java
@@ -11,5 +11,6 @@ public class Main {
             s.area();
             s.describe();
         }
+
     }
 }
--- week-03-oop/src/ShapesApp/Shape.java
+++ week-03-oop/src/ShapesApp/Shape.java
@@ -1,7 +1,7 @@
 package ShapesApp;
 
-public class Shape {
-    public double area() { return 0; }
+public abstract class Shape {
+    public abstract double area();
     public void describe() {
         System.out.println("This is a shape");
     }
